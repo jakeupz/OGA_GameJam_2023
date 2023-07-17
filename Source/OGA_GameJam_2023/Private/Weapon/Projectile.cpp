1e9c046ffd77084116b9e8b98bb411fd53a3ffdc
bacon <50970307+jakeupz@users.noreply.github.com>
2023-07-17 11:19:47 -0500

    LET THIS WORKKKKKKKK

--- Source/OGA_GameJam_2023/Private/Weapon/Projectile.cpp
+++ Source/OGA_GameJam_2023/Private/Weapon/Projectile.cpp
@@ -9,9 +9,6 @@
 #include "Particles/ParticleSystem.h"
 #include "Sound/SoundCue.h"
 #include "Character/PlayerCharacter.h"
-#include "Components/PrimitiveComponent.h"
-#include "GameFramework/Actor.h"
-
 
 AProjectile::AProjectile()
 {
